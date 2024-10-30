f472b7313b5d3936dc3f50c0bf17f74e8ec4c441
AlexRamosGlz <joralexfx@gmail.com>
2024-10-30 13:54:41 -0600

    html course refactor

--- React/components/src/components/ProgressBar/Step/Step.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Step({ index }) {
-  return (
-    <div className="step">
-      <p className="index">{index.toString().length <= 1 ? `0${index}` : index}</p>
-    </div>
-  );
-}
--- React/section_2/travel-list/src/components/Logo.jsx
+++
@@ -1,3 +0,0 @@
-export function Logo() {
-    return <h1>🌴 Far Away 💼</h1>;
-}
